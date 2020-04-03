63eb499d9c0225c8559ec9b34e2e561f1a747eaa
kevinanggaaa <60419316+kevinanggaaa@users.noreply.github.com>
2020-04-03 12:54:35 +0700

    Update soal4a.c

--- soal4/soal4a.c
+++ soal4/soal4a.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h> //library thread
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <unistd.h>
 
 void *print_message_function( void *ptr );
 void *calculateMatrix( void *ptr );
@@ -13,14 +10,14 @@ void *calculateMatrix( void *ptr );
 // Make sure it fulfils the multiplication rules of matrices
 int matrix1[4][2] = {
   1, 2,
-  4, 6, 
-  3, 12,
-  1, 7
+  3, 4,
+  5, 6,
+  7, 8 
 };
 
 int matrix2[2][5] = {
-  1, 2, 4, 5, 6,
-  4, 5, 4, 3, 2 
+  9, 10, 11, 12, 13,
+  14, 15, 16, 17, 18
 };
 
 // size of matrice
@@ -28,21 +25,18 @@ int mat1I = 4, mat1J = 2;
 int mat2I = 2, mat2J = 5;
 // values to be returned by thread processes
 // int retVals[mat1I][mat2J];
-int retVals[2][5];
+int retVals[4][5];
 // semaphone for retMatrix
 int retMatLock = 0;
 
 
 int main()
 {
-  key_t key = 1234;
-  int *retVals[4][5];
   pthread_t thread[mat1I][mat2J];//inisialisasi awal
   int iret[mat1I][mat2J];
   
-  int shmid = shmget(key, sizeof(int[4][5]), IPC_CREAT | 0666);
-  retVals[4][5] = (int *)shmat(shmid, NULL, 0);
- // Dispatch a single thread for every element in result matrix
+
+  // Dispatch a single thread for every element in result matrix
   int i, j;
   for (i = 0; i < mat1I; i++) {
     for (j = 0; j < mat2J; j++) {
@@ -53,7 +47,7 @@ int main()
       void *msgPtr = (void *)message;
       iret[i][j] = pthread_create( &thread[i][j], NULL, calculateMatrix, msgPtr); //membuat thread pertama
       if(iret[i][j]) //jika eror
-      {
+{
           fprintf(stderr,"Error - pthread_create() return code: %d\n",iret[i][j]);
           exit(EXIT_FAILURE);
       }
@@ -61,6 +55,7 @@ int main()
       // printf("pthread_create() for thread 1 returns: %d\n", iret[i][j]);
     }
   }
+
   for (i = 0; i < mat1I; i++) {
     for (j = 0; j < mat2J; j++) {
       pthread_join( thread[i][j], NULL );
@@ -74,12 +69,9 @@ int main()
       printf(", %d", retVals[i][j]);
     }
     printf("\n");
-  shmdt(retVals);
- shmctl(shmid, IPC_RMID, NULL);
   }
   exit(EXIT_SUCCESS);
 }
-
 void *calculateMatrix( void *temp) {
   int *message;
   message = (int *)temp;
@@ -94,6 +86,7 @@ void *calculateMatrix( void *temp) {
     iMat1, jMat2, matrix1[iMat1][i], matrix2[i][jMat2], sum);
     sum += matrix1[iMat1][i] * matrix2[i][jMat2];
   }
+
   // while(retMatLock) {
   //   _sleep(1);
   // }
